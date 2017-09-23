a79bb38c0afc209dc0955db9c0f28a5217d21fab
Mateusz Kubiak <mateusz.kubiak@atos.net>
2017-09-23 16:10:22 +0200

    remove unused files

--- src/main/java/net/atos/itdays/domain/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package net.atos.itdays.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import net.atos.itdays.domain.Event;
-
-public interface EventRepository extends CrudRepository<Event, Long>{
-
-}
